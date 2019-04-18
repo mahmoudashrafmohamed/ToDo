419e9dbc7e026c2a67986d99af23d340c5eb482b
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2019-04-17 20:25:32 -0700

    add DAO interface

---
+++ app/src/main/java/com/example/mahmoud_ashraf/todo/data/db/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.mahmoud_ashraf.todo.data.db
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+/**
+ * Created by mahmoud_ashraf on 17,04,2019
+ *  This is where the database (create, read, update and delete) operations are defined
+ *  don't forget annotation :)
+ */
+@Dao
+interface NoteDao {
+    @Insert
+    fun insert(note: Note)
+
+    @Query("DELETE FROM notes_table")
+    fun deleteAllNotes()
+
+    @Query("SELECT * FROM notes_table ")
+    fun getAllNotes(): LiveData<List<Note>>
+}
